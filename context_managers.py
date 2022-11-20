a7aa12e7499cd47e402c671efc798b41a5612a3a
wsy <tahoeivanova@gmail.com>
2022-11-20 21:34:54 +0300

    1st commit

---
+++ context_managers.py
@@ -0,0 +1,19 @@
+"""
+Context managers
+"""
+class ManagedFile:
+    """Context manager that opens and closes file after writing to it"""
+    def __init__(self, name):
+        self.name = name
+
+    def __enter__(self):
+        self.file = open(self.name, 'w')
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.file:
+            self.file.close()
+
+with ManagedFile('hello.txt') as f:
+    f.write("hello ")
+    f.write("world!")
